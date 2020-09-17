0d4aeb6af8f27d3e1d3f0726ed753c03a901f595
Tori Rossini <tori.rossini42@gmail.com>
2020-09-17 15:04:58 -0400

    Drawing circles works - need to fix the spread

--- src/abcLIB.js
+++ src/abcLIB.js
@@ -3,8 +3,24 @@
         console.log("meh");
     }
     
+    function dtr(degrees){
+            return degrees * (Math.PI/180);
+        }
+
+    function drawCircle(ctx,x,y,radius,color){
+        ctx.save();
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(x,y,radius,0,Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+        ctx.restore();
+    }
+    
     const abcLIB = {
-        doStuff:doStuff
+        doStuff:doStuff,
+        dtr:dtr,
+        drawCircle:drawCircle
     };
     
     window.abcLIB = abcLIB;
