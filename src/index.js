d2648ae0d23b2e20dd3805068f2f33d9ad1ded6a
Tori Rossini <tori.rossini42@gmail.com>
2020-09-24 14:38:03 -0400

    Finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+        
+(function(){
         "use strict";
         const canvasWidth = 600, canvasHeight = 500;
         let ctx;
@@ -6,15 +8,25 @@
         
         let flowers=[];
 
-        let colorStyle = "green";
+        let colorStyle = "hsl";
         let backgroundColor = "black";
         
         let fps = 120;
         let maxIterations, divergence, c;
+
+        let defaultSettings = [50, 5, 120, 4, "black", "hsl"];
         
         window.onload = init;
         
         function init(){
+            document.querySelector("#exportButton").onclick = doExport;
+            
+            document.querySelector("#clearButton").onclick = clear;
+            
+            document.querySelector("#resetButton").onclick = function(){
+                setAll([50, 5, 120, 4, "black", "hsl"]);
+            };
+            
             ctx = canvas.getContext("2d");
             canvas.width = canvasWidth;
             canvas.height = canvasHeight;
@@ -61,6 +73,25 @@
                 console.log("changed background to " + backgroundColor);
             };
             
+            document.querySelector("#chooserPresets").onchange = function(e){
+                if (this.value == "spokes") {
+                    setAll([140, 60, 120, 5]);
+                }
+                if (this.value == "whirlpool") {
+                    setAll([160, 70, 120, 4]);
+                }
+                if (this.value == "spots") {
+                    setAll([100, 100, 100, 20]);
+                }
+                if (this.value == "firework") {
+                    setAll([430,100,1000,5]);
+                }
+                if (this.value == "default") {
+                    setAll([50,5,120,4]);
+                }
+                
+                console.log("changed preset to " + this.value);
+            }
             document.querySelector("#chooserColorStyle").onchange = function(e){
                 colorStyle = this.value;
                 console.log("changed color to " + colorStyle);
@@ -76,6 +107,44 @@
             loop();
             canvas.onclick = canvasClicked;
 		}
+
+        function doExport(){
+          // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL
+          // https://www.w3schools.com/jsref/met_win_open.asp
+          const data = canvas.toDataURL(); 
+          const newWindow = window.open();
+          newWindow.document.body.innerHTML = `<iframe src="${data}" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>`;
+        }
+
+        function clear(){
+            ctx.save();
+            ctx.fillStyle = backgroundColor;
+            ctx.fillRect(0,0,canvasWidth,canvasHeight);
+            ctx.restore();
+        }
+
+        function setAll(setArray)
+        {
+            console.log("Setting values to: " + setArray[0] + "  " + setArray[1] + "  " + setArray[2] + "  " + setArray[3] + "  " + setArray[4] + "  " + setArray[5]);
+            maxIterations = setArray[0];
+            maxIterationSlider.value = maxIterations;
+            maxIterationOutput.innerHTML = maxIterationSlider.value;
+            
+            divergence = setArray[1];
+            divergenceSlider.value = divergence;
+            divergenceOutput.innerHTML = divergenceSlider.value;
+            
+            
+            fps = setArray[2];
+            fpsSlider.value = fps;
+            fpsOutput.innerHTML = fpsSlider.value;
+            
+            
+            c = setArray[3];
+            cSlider.value = c;
+            cOutput.innerHTML = cSlider.value;
+            
+        }
 
         function canvasClicked(e){
             let rect = e.target.getBoundingClientRect();
@@ -108,7 +177,7 @@
           }
 
         class Botany{
-            constructor(x,y,maxInterations = 100, color = "green", fps = 120, divergence = 5, c = 4){
+            constructor(x,y,maxInterations = 100, color = "hsl", fps = 120, divergence = 5, c = 4){
                 this.x = Number(x);
                 this.y = Number(y);
                 this.color = color;
@@ -156,15 +225,17 @@
 
     }
         
-        function loop(){
+    function loop(){
 
-            for(var i = 0; i < flowers.length; i++)
-                {
-                    if (flowers[i].n < flowers[i].maxInterations)
-                        {
-                            flowers[i].drawBotany();
-                        }
-                }
-            
-            setTimeout(loop, 1000/fps);      
-        }
+        for(var i = 0; i < flowers.length; i++)
+            {
+                if (flowers[i].n < flowers[i].maxInterations)
+                    {
+                        flowers[i].drawBotany();
+                    }
+            }
+
+        setTimeout(loop, 1000/fps);      
+    }
+    
+})();
